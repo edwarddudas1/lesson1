8dc8fbdc6d91c8d575855258ef30443d3b732562
edwarddudas1 <edwarddudas@gmail.com>
2025-05-11 10:00:52 +0300

    Done homework

--- lesson2HW/Homework2/src/App.jsx
+++ lesson2HW/Homework2/src/App.jsx
@@ -4,21 +4,21 @@ import Greeting from './Greeting'
 import Message from './Message'
 import Button from './Button'
 
-
 function App() {
+const handleClick = () => {
+    console.log("Кнопка натиснута")
+  }
 
-  const handleClick = () => {
-      alert("Кнопка натиснута")
-    }
-  };
 
-  return (
-    <>
-      <Greeting name="Іван"/>
-      <Message text="2"/>
-      <Button onClick={handleClick}></Button>
-    </>
-  )
 
+
+return (
+  <>
+    <Greeting name="Іван"/>
+    <Message text="2"/>
+    <Button onClick={handleClick}/>
+  </>
+);
+};
 
 export default App
