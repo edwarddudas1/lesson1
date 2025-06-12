d5acdfe376fcbace5e4e78d539ea8f9ff903f27f
edwarddudas1 <edwarddudas@gmail.com>
2025-06-12 20:11:43 +0300

    fix

--- Lesson5/vite-project/src/components/Component.jsx
+++ Lesson5/vite-project/src/components/Component.jsx
@@ -1,28 +1,56 @@
 import { Component } from "react";
 
-export default class Hello extends Component {
-    constructor(props) {
-        super(props) 
-        this.state = {
-            count: 0
-        }
-    }
+// export default class Hello extends Component {
+//     constructor(props) {
+//         super(props) 
+//         this.state = {
+//             count: 0
+//         }
+//     }
 
-    handleIncrementClick = () => {
-        this.setState({count: this.state.count + 1})
-    }
+//     handleIncrementClick = () => {
+//         this.setState({count: this.state.count + 1})
+//     }
 
-    handleDecrementClick = () => {
-        this.setState({count: this.state.count - 1})
-    }
+//     handleDecrementClick = () => {
+//         this.setState({count: this.state.count - 1})
+//     }
     
-  render() {
+//   render() {
+//     return (
+//       <>
+//         <h1>Counter's number is</h1>
+//         <p>{this.state.count}</p>
+//         <button onClick={this.handleIncrementClick}>Increment</button>
+//         <button onClick={this.handleDecrementClick}>Decrement</button>
+//       </>
+//     );
+//   }
+// }
+
+export default class SecretMessage  extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      isVisible: true
+    }
+  }
+ShowOrDissapear = () => {
+  this.state.isVisible({isVisible: !this.state.isVisible});
+}
+
+
+
+    render() {
+      
+
+
     return (
       <>
-        <h1>Counter's number is</h1>
+        <h1>Temprature</h1>
         <p>{this.state.count}</p>
-        <button onClick={this.handleIncrementClick}>Increment</button>
-        <button onClick={this.handleDecrementClick}>Decrement</button>
+        <h2>Секретне повідомлення</h2>
+        <button onClick={this.ShowOrDissapear}>Показати/Сховати</button>
       </>
     );
   }
