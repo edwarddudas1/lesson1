5d9d768c04d0fa3370ce232fcdee961d771a2f6c
edwarddudas1 <edwarddudas@gmail.com>
2025-06-19 18:51:02 +0300

    finnal work with 5HW

--- goiteens-events-js/src/components/Event.jsx
+++ goiteens-events-js/src/components/Event.jsx
@@ -1,14 +1,18 @@
 import styled from 'styled-components';
+import { FaMapMarkerAlt, FaUser, FaClock, FaPlay, FaStop } from 'react-icons/fa';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  background-color:rgb(104, 104, 104);
+  
 `;
 
 const Box = styled.div`
   background-color:rgb(240, 208, 160);
-  border: 4px solid rgb(222, 178, 120);
+  border: 5px solid rgb(222, 178, 120);
   width: 500px;
+
 `
 
 const Title = styled.h2`
@@ -22,15 +26,17 @@ const Text = styled.p`
 
 export default function Event({ name, location, speaker, type, start, end }) {
   return (
-    <Container>
-        <Box>
-          <Title>{name}</Title>
-          <Text>Location: {location}</Text>
-          <Text>Speaker: {speaker}</Text>
-          <Text>Type: {type}</Text>
-          <Text>Start: {start}</Text>
-          <Text>End: {end}</Text>
-        </Box>
-    </Container>
+    <>
+      <Container>
+          <Box>
+            <Title> {name}</Title>
+            <Text><FaMapMarkerAlt /> {location}</Text>
+            <Text><FaUser /> {speaker}</Text>
+            <Text><FaClock /> {type}</Text>
+            <Text><FaPlay /> Start: {start}</Text>
+            <Text><FaStop /> End: {end}</Text>
+          </Box>
+      </Container>
+    </>
   );
 }
--- goiteens-events-js/src/components/PageBoard.jsx
+++ goiteens-events-js/src/components/PageBoard.jsx
@@ -2,33 +2,46 @@ import styled from "styled-components";
 import Event from "./Event";
 import eventsData from "../upcoming-events.json";
 
+
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
   flex: 1 1; 
+  background-color:rgb(179, 179, 179);
+  border: 5px solid rgb(131, 131, 131);
+  border-radius: 15px;
+
 `
 const Box = styled.ul`
- 
+margin-left: 50px;
+margin-top: 50px;
+margin-bottom: 50px;
 `
+const Title = styled.h2`
+  color: black;
+  font-weight: 600;
+  background-color: rgb(124, 196, 244);
+  border: 5px solid rgb(65, 138, 248);
+`;
 
 
 export default function PageBoard({ events = eventsData }) {
   return (
     <>
+    <Title>24th Core Worlds Coalition Conference</Title>
       <Container>
-        <Box>
-          {events.map((event, index) => (
-            <Event
-              key={index}
-              name={event.name}
-              start={event.time.start}
-              end={event.time.end}
-              location={event.location}
-              speaker={event.speaker}
-            />
-          ))}
+      {events.map((event, index) => (
+        <Box key={index}>
+          <Event
+            name={event.name}
+            start={event.time.start}
+            end={event.time.end}
+            location={event.location}
+            speaker={event.speaker}
+          />
         </Box>
-      </Container>
+      ))}
+    </Container>
     </>
   );
 }
