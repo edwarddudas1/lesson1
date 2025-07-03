fff9f5caa5f47df4159599dbc8ce24e54c4707e5
edwarddudas1 <edwarddudas@gmail.com>
2025-07-03 19:50:50 +0300

    add handleSubmit

--- Lesson10/vite-project/src/components/TodoEditor.jsx
+++ Lesson10/vite-project/src/components/TodoEditor.jsx
@@ -4,9 +4,17 @@ export default class TodoEditor extends Component {
     state={
         textValue: '',
     }
+    handleChange = (e) => {
+        this.setState({textValue: e.target.value})
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const {textValue} = this.state
+        console.log(textValue)
+    }
     render(){return(
-        <form>
-            <input type="text" />
+        <form onSubmit={this.handleSubmit}>
+            <input type="text" onChange={this.handleChange} value={this.state.textValue}/>
             <button>Додати</button>
         </form>
     )}
