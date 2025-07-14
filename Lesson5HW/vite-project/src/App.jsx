dc4572986aa509dc8ab15ea738a1c217a9dd2307
edwarddudas1 <edwarddudas@gmail.com>
2025-07-14 13:04:47 +0300

    add hw 5

---
+++ Lesson5HW/vite-project/src/App.jsx
@@ -0,0 +1,22 @@
+import Recipe from './Recipe';
+import recipesData from './Recipies';
+// import { IoTimer } from "react-icons/io5";
+// import { FaFire } from "react-icons/fa6";
+
+function App() {
+  
+  let iconsData = {
+    time: <IoTimer/>,
+    calories: <FaFire/>,
+  }
+
+  return (
+    <div>
+      {recipesData.map((recipe, index) => (
+        <Recipe key={index} recipe={recipe} icon={iconsData}/>
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ Lesson5HW/vite-project/src/Recipies.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import RecipeInfo from "./RecipeInfo";
+import PropTypes from "prop-types";
+const Title = styled.h1`
+font-size: 180px;`
+
+export default function Recipe({ recipe, icon }) {
+    const { name, time, servings, calories, difficulty, image } = recipe;
+
+    return (
+      <>
+      <div className="recipe-container">
+       <div className="text">
+        <Title>Hello</Title>
+        <p>{name}</p>
+        </div>
+        <div className="image">
+        <img src={image} alt="image" width="300" />
+        </div>
+        <RecipeInfo text={time} icon={icon.time} />
+        <RecipeInfo text={servings} />
+        <RecipeInfo text={calories} icon={icon.calories} />
+        <RecipeInfo text={difficulty} />
+        </div>
+      </>
+    );
+}
+Recipe.propTypes = {
+  recipe: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    time: PropTypes.string.isRequired,
+    servings: PropTypes.number.isRequired,
+    calories: PropTypes.number.isRequired,
+    difficulty: PropTypes.number.isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+  icon: PropTypes.shape({
+    time: PropTypes.element.isRequired,
+    calories: PropTypes.element.isRequired,
+  }).isRequired,
+};
